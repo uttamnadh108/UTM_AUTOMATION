afbfd5b6dab0ccdcba8c03f89ecc35bcab2cae84
UttamnadH <uttamnadh@gmail.com>
2024-10-07 15:51:29 +0530

    calc program

---
+++ src/Oct/ex_02102024/Calc.java
@@ -0,0 +1,16 @@
+package Oct.ex_02102024;
+
+public class Calc {
+public static void main (String args[])
+{
+    int a=10;
+    int b =10;
+    System.out.println(a*b);
+    System.out.println(a-b);
+    System.out.println(a+a);
+    System.out.println(a/b);
+}
+
+
+
+}
