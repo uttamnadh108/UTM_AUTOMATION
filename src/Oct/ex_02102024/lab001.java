fc7279bbe5fdb1110f3617191371002444b07db7
UttamnadH <uttamnadh@gmail.com>
2024-10-14 07:26:11 +0530

    practice program

---
+++ src/Oct/ex_02102024/lab001.java
@@ -0,0 +1,9 @@
+package Oct.ex_02102024;
+
+public class lab001 {
+    public static void main(String arg[]){
+        System.out.println("Hello uttam");
+        System.out.println("pls. practice");
+    }
+
+}
---
+++ src/Oct/ex_03012024/lab002_comments.java
@@ -0,0 +1,12 @@
+package Oct.ex_03012024;
+/*
+
+multiline comments
+can be used to with astrict and slash
+
+ */
+public class lab002_comments{
+    public static void main(String[] args){
+        System.out.println("singlelinecomment");//single line comment
+    }
+}
